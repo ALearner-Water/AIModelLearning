d3b61631197b81fc6711d0a0d3bcd8137dbdcf0f
ALearner-Water <535070466@qq.com>
2026-09-15 21:34:53 +0800

    完整使用softmax写了一个参数更新过程

--- Pytorch_basic/softmax/Fasion_mnist.py
+++ Pytorch_basic/softmax/Fasion_mnist.py
@@ -23,8 +23,8 @@ for x,y in train_loader:
     continue
 end=time.time()
 print(end-start)    
-#===============================================================处理阶段 从零实现
-num_inputs=764  #因为图片是28*28的灰度图片，这里需要把他拉长成一维向量
+# ===============================================================处理阶段 从零实现
+num_inputs=784  #因为图片是28*28的灰度图片，这里需要把他拉长成一维向量
 num_outputs=10  #一共有十个类别，最后让神经网络输出10个分数
 w=torch.normal(0,0.01,size=(num_inputs,num_outputs),requires_grad=True) #从均值为0 标准差为0.01的正态分布随机采样
 b=torch.zeros(10,requires_grad=True)
@@ -33,3 +33,17 @@ def softmax(x):
     x_exp=torch.exp(x)  #分子 
     partition=x_exp.sum(1,keepdim=True) #分母
     return x_exp/partition  #这个就是softmax求概率
+def net(x):
+    #求y_hat
+    return softmax(x.reshape((-1,w.shape[0]))@w+b)
+def cross_entropy(y_hat,y):
+    #根据下标拿值求交叉熵   
+    return -torch.log(y_hat[range(len(y_hat)),y])   #在y_hat每一行就是一个样本中拿出正确的类别的值 y就是正确类别的下标 -log y_hat就是交叉熵
+def train(optimizer,net,cross_entropy,epochs,train_loader):
+    for i in range(epochs):
+        for batch_x,batch_y in train_loader:
+            batch_y_hat=net(batch_x)
+            loss=cross_entropy(batch_y_hat,batch_y).mean()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
